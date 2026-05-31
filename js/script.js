32d5322b3aa78dc3d5215a08520ee5bd96da5829
hantie <262029@virtualwindow.co.za>
2026-05-31 19:00:26 +0200

    Form modal update and id for adopt pages
    updated modal to be functioning based on class walkthrough
    and added id's to all adopt cards

--- js/script.js
+++ js/script.js
@@ -1,63 +1,63 @@
-// Form Time !!
+// Contact Form Time !!
 // Get Form data
 
-let form = document.forms['filledForm'];
-form.addEventListener('submit', getFormInfo);
+contactFormFunction = () => {
+    let form = document.forms['filledForm'];
+    form.addEventListener('submit', getFormInfo);
 
-// Extract the Form data into an object
-function getFormInfo(event){
-    // Denying of placeholder text
-    event.preventDefault();
+    // Extract the Form data into an object
+    function getFormInfo(event){
+        // Denying of placeholder text
+        event.preventDefault();
 
-    // Create the object that will store the form data
-    let formData = {
-        'name' : this['name'].value,
-        'email' : this['email'].value,
-        'subject' : this['subject'].value,
-        'message' : this['message'].value
+        // Create the object that will store the form data
+        let formData = {
+            'name' : this['name'].value,
+            'email' : this['email'].value,
+            'subject' : this['subject'].value,
+            'message' : this['message'].value
+        };
+
+        // Write the HTML to the variable to be interpolated
+        let output = `
+            <p>
+                Thank you, <span>${formData['name']}</span> for your message!
+            </p>
+            <p>
+                We will contact you soon
+            </p>
+        `;
+
+        // Select the HTML Section to update
+        let outSection = document.querySelector('.outputContainer');
+
+
+        // Get and populate a Modal from bootstrap and show
+        // Populating the section of the bootstrap modal
+        outSection.innerHTML = output;
+
+        // Get corresponding HTML element
+        let formModal = new bootstrap.Modal(
+            document.getElementById('formModal')
+        );
+
+        // Show Modal
+        formModal.show();
+
+        // Clear Form
+        form.reset();
     };
-
-    // Write the HTML to the variable to be interpolated
-    let output = `
-        <p>
-            Thank you, <span>${formData['name']}</span> for your message!
-        </p>
-        <p>
-            We will contact you soon
-        </p>
-    `;
-
-    // Select the HTML Section to update
-    let outSection = document.querySelector('.outputContainer');
-
-
-    // Get and populate a Modal from bootstrap and show
-    // Populating the section of the bootstrap modal
-    outSection.innerHTML = output;
-
-    // Get corresponding HTML element
-    let formModal = new bootstrap.Modal(
-        document.getElementById('formModal')
-    );
-
-    // Show Modal
-    formModal.show();
-
-    // Clear Form
-    form.reset();
 };
 
-
-
 // Counter
-// Using a singular function to do the counting,
-// I feel like the should be a more efficient way of doing it tho
+// Used for add and subtract buttons on adopt page and in cart
 countMe = (operator, petID) => {
     // Extract element from HTML
     let num = document.getElementById(petID).value;
 
+    // Minus, making
     if (operator == 'minus' ){
-        if (num > 0){
+        if (num > 1){
             num -=1;
         }
     };
